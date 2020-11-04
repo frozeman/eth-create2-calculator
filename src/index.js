06c7c8040d47e9a9df4e211bf73da4bf7f8d375e
Fabian Vogelsteller <fabian@frozeman.de>
2020-11-04 10:36:38 +0100

    init commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const {id, keccak256, defaultAbiCoder, getAddress, getCreate2Address} = require('ethers/lib/utils')
+
+const saltToHex = (salt) => {
+    salt = salt.toLowerCase()
+    return (salt.indexOf("0x") !== -1)
+        ? salt
+        : id(salt.toString())
+}
+
+const encodeParams = (dataTypes, data) => {
+    return defaultAbiCoder.encode(dataTypes, data)
+}
+
+const buildBytecode = (
+    constructorTypes,
+    constructorArgs,
+    contractBytecode,
+) => {
+    return `${contractBytecode}${encodeParams(constructorTypes, constructorArgs).slice(
+        2,
+    )}`
+}
+
+exports.calculateCreate2 = (from, salt, byteCode, constructorArgs) => {
+
+    // add constructor arguments manually, if present
+    if(constructorArgs)
+        byteCode = buildBytecode(constructorArgs.types, constructorArgs.params, byteCode)
+
+    const saltHex = saltToHex(salt)
+
+    return getCreate2Address(from, saltHex, keccak256(byteCode))
+        // .slice(-40)}`.toLowerCase()) return getAddress(`0x${keccak256(
+        //     `0x${['ff', from, saltHex, keccak256(byteCode)]
+        //         .map((x) => x.replace(/0x/, ''))
+        //         .join('')}`,
+        // )
+        // .slice(-40)}`.toLowerCase())
+}
